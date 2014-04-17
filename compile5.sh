6f9f19c5373a3c32a584fdffab74e30b0f9c42b4
Andrey Pangin <andrei.pangin@odnoklassniki.ru>
2014-04-18 02:10:29 +0400

    Removed old files

--- compile5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-JAVA_HOME=/usr/java/jdk1.7.0_40
-gcc -g -O2 -fPIC -shared -olib/libdistance.so -Wl,-soname,libdistance.so -I $JAVA_HOME/include -I $JAVA_HOME/include/linux src/demo5/distance.c
--- debug6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$PWD/lib
-java -XX:CompileCommandFile=src/demo6/.hotspot_compiler -cp out/production/crash $* demo6.Malloc
